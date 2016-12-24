f9ea8aed4ef1f424467bc783b5aa2c23a5543d19
gcj <longgongsanri@163.com>
2016-12-24 18:36:04 +0800

    toolbar右上角的菜单

--- app/src/main/java/tk/chuanjing/moretest/MainActivity.java
+++ app/src/main/java/tk/chuanjing/moretest/MainActivity.java
@@ -14,6 +14,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.Toast;
 
 import tk.chuanjing.moretest.testrecyclerview.RecyclerviewActivity;
 
@@ -81,12 +82,20 @@ public class MainActivity extends AppCompatActivity
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Toast.makeText(getApplicationContext(), "settings", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_send:
+                Toast.makeText(getApplicationContext(), "send", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_share:
+                Toast.makeText(getApplicationContext(), "share", Toast.LENGTH_SHORT).show();
+                break;
         }
+
+
+
 
         return super.onOptionsItemSelected(item);
     }
