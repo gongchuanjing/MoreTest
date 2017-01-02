f8c57e5218803e4a83747d15ed55891f30faff13
gcj <longgongsanri@163.com>
2017-01-02 16:42:08 +0800

    添加万能刷新头：SwipeRefreshLayout

--- app/src/main/java/tk/chuanjing/moretest/testrecyclerview/RecyclerViewCardViewActivity.java
+++ app/src/main/java/tk/chuanjing/moretest/testrecyclerview/RecyclerViewCardViewActivity.java
@@ -1,6 +1,7 @@
 package tk.chuanjing.moretest.testrecyclerview;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -10,9 +11,13 @@ import java.util.List;
 
 import tk.chuanjing.moretest.R;
 
-public class RecyclerViewCardView extends AppCompatActivity {
+import static tk.chuanjing.moretest.R.id.recyclerView;
+import static tk.chuanjing.moretest.R.id.refresh;
+
+public class RecyclerViewCardViewActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
 
     private List<TestBean> testList;
+    private SwipeRefreshLayout refresh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +25,16 @@ public class RecyclerViewCardView extends AppCompatActivity {
         setContentView(R.layout.activity_recyclerview_and_cardview);
 
         initDate();
+
+        //下拉刷新
+        refresh = (SwipeRefreshLayout) findViewById(R.id.refresh);
+        refresh.setOnRefreshListener(this);
+        refresh.setColorSchemeResources(android.R.color.holo_red_dark,
+                android.R.color.holo_green_dark,
+                android.R.color.holo_blue_dark,
+                android.R.color.holo_orange_dark);
+        refresh.setProgressBackgroundColor(R.color.colorPrimary);
+
 
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         GridLayoutManager layoutManager = new GridLayoutManager(this, 3);
@@ -38,5 +53,20 @@ public class RecyclerViewCardView extends AppCompatActivity {
             testList.add(new TestBean("心", R.mipmap.icon_xin));
             testList.add(new TestBean("延", R.mipmap.icon_yan));
         }
+    }
+
+    @Override
+    public void onRefresh() {
+        // 加载数据
+        // 加载数据完成，隐藏刷新头
+        // refresh.setRefreshing(false);
+
+        // 这里延迟三秒隐藏刷新头，模拟加载数据
+        refresh.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                refresh.setRefreshing(false);
+            }
+        }, 5000);
     }
 }
--- app/src/main/java/tk/chuanjing/moretest/testrecyclerview/RecyclerviewActivity.java
+++ app/src/main/java/tk/chuanjing/moretest/testrecyclerview/RecyclerviewActivity.java
@@ -3,7 +3,6 @@ package tk.chuanjing.moretest.testrecyclerview;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.CardView;
 import android.view.View;
 import android.widget.Button;
 
@@ -39,7 +38,7 @@ public class RecyclerviewActivity extends AppCompatActivity {
         btn_recyclerview_cardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), RecyclerViewCardView.class));
+                startActivity(new Intent(getApplicationContext(), RecyclerViewCardViewActivity.class));
             }
         });
     }
