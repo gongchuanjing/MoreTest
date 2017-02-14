356298edea122b7eaf59ec39c15644c33e454d86
gcj <longgongsanri@163.com>
2017-02-14 18:01:36 +0800

    CallbackToBean中增加noDataCode

--- app/src/main/java/tk/chuanjing/moretest/testokhttp/CallbackToBean.java
+++ app/src/main/java/tk/chuanjing/moretest/testokhttp/CallbackToBean.java
@@ -2,6 +2,7 @@ package tk.chuanjing.moretest.testokhttp;
 
 import android.os.Handler;
 import android.os.Message;
+import android.text.TextUtils;
 
 import java.lang.reflect.ParameterizedType;
 
@@ -15,34 +16,40 @@ import tk.chuanjing.moretest.MyApp;
 /**
  * Created by ChuanJing on 2017/2/9.
  * 希望在请求的execute中传入这个类，成功直接就能返回解析后的对象，
- * 如果请求失败就直接在这里提示，适用于有正规的数据返回结构，如：
- * {
-        "message": "OK", // 消息提示
-        "retcode": 200,  // 提示编码
-        "data": [
-            { }
-        ]
-    }
+ * 如果请求失败，能在这里处理掉就直接在这里处理
  */
 
 public class CallbackToBean<T> extends Callback<T> {
 
     private Handler handler;
-    private int sucCode;
+    private int successCode;
+    private int noDataCode;
 
-    public CallbackToBean(Handler handler, int sucCode) {
+    public CallbackToBean(Handler handler, int successCode) {
         this.handler = handler;
-        this.sucCode = sucCode;
+        this.successCode = successCode;
+    }
+
+    public CallbackToBean(Handler handler, int successCode, int noDataCode) {
+        this.handler = handler;
+        this.successCode = successCode;
+        this.noDataCode = noDataCode;
     }
 
     @Override
     public T parseNetworkResponse(Response response, int id) throws Exception {
         String string = response.body().string();
 
-//        if (TextUtils.isEmpty(string) || "[]".equals(string)||"{}".equals(string)) {
-//            ToastUtils.showMyToast(MyApp.getInstance(), "服务器返回数据为空");
-//            return null;
-//        }
+        // 如果请求回来没有数据，直接在这里提示，也可在外面处理，接收handler的noDataCode即可处理
+        if (TextUtils.isEmpty(string) || "[]".equals(string)||"{}".equals(string)) {
+            ToastUtils.showMyToast(MyApp.getInstance(), "服务器返回数据为空");
+            if (noDataCode != 0) {
+                Message msg = handler.obtainMessage();
+                msg.what = noDataCode;
+                handler.handleMessage(msg);
+            }
+            return null;
+        }
 
         Class<T> entityClass = (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
         if (entityClass == String.class) {
@@ -62,7 +69,7 @@ public class CallbackToBean<T> extends Callback<T> {
     @Override
     public void onResponse(T response, int id) {
         Message msg = handler.obtainMessage();
-        msg.what = sucCode;
+        msg.what = successCode;
         msg.obj = response;
         handler.handleMessage(msg);
     }
